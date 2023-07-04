325b6f06e6d5f938449afa8274b1c345c332a481
Vieira, Joao <jvr.vieira@gmail.com>
2023-07-04 16:09:11 -0300

    #1: create files

---
+++ infra/db.tf
@@ -0,0 +1,25 @@
+data "aws_secretsmanager_secret" "secrets" {
+  arn = "arn:aws:secretsmanager:us-east-1:798616997032:secret:db_credentials-itNIU7"
+}
+
+data "aws_secretsmanager_secret_version" "current" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
+}
+
+resource "aws_db_instance" "captainoftheseas" {
+  allocated_storage   = 20
+  engine              = "postgres"
+  engine_version      = "15.3"
+  identifier          = "captainoftheseas"
+  instance_class      = "db.t3.micro"
+  skip_final_snapshot = true
+  storage_encrypted   = false
+  publicly_accessible = false
+  apply_immediately   = true
+
+  password = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["password"]
+  username = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["username"]
+  tags = {
+    project = "captainofthesea"
+  }
+}
---
+++ infra/secrets.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "db_address" {
+  name        = "/database/endpoint"
+  description = "Database Endpoint"
+  type        = "SecureString"
+  value       = aws_db_instance.captainoftheseas.endpoint   
+
+  tags = {
+    environment = "production"
+    project = "captainofthesea"
+  }
+}
