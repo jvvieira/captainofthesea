85f473da92bfacd456ee2fc48c7db8e92a4ecb73
Vieira, Joao <jvr.vieira@gmail.com>
2023-07-13 09:16:45 -0300

    clean backend

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "db_address" {
   name        = "/database/endpoint"
   description = "Database Endpoint"
   type        = "SecureString"
-  value       = aws_db_instance.captainoftheseas.endpoint   
+  value       = aws_db_instance.captainoftheseas.endpoint
 
   tags = {
     environment = "production"
