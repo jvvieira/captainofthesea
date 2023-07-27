f620f2efc4c18f13aa5ca3682ed2b55ae87829f1
Vieira, Joao <jvr.vieira@gmail.com>
2023-07-27 15:26:27 -0300

    https permission

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -8,6 +8,7 @@ resource "aws_security_group" "main" {
     protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -18,6 +19,20 @@ resource "aws_security_group" "main" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
